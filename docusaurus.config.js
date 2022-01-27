91c444abe944a3f1d83031f3cbadf1dda9c2eabd
anattaguo <mengjunkwok@gmail.com>
2022-01-27 18:31:03 +0800

    fix error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: '简疏志',
   tagline: '常精进，勿懈怠',
-  url: 'https://jianshuzhi.netlify.app',
+  url: 'https://jianshuzhi.me',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
